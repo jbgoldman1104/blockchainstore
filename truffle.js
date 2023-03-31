f83325ddd555e852b60a0dbd02e87b0e4b5069b7
jbgoldman1104 <jbgoldman1104@gmail.com>
2023-03-30 20:37:23 -0800

    "updated on 2023-03-30"

--- truffle.js
+++ truffle.js
@@ -3,4 +3,10 @@ require('babel-register')
 
 module.exports = {
   networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    }
+  }
 }
